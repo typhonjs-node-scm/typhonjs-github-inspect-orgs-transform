7162590ea4a7648b8acf9b49a5e9d38785ccac7d
typhonrt <support@typhonrt.org>
2016-03-04 05:08:05 -0800

    0.0.3 release

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,8 +2,8 @@
  * Please see `typhonjs-core-gulptasks` (https://www.npmjs.com/package/typhonjs-core-gulptasks)
  */
 
-import gulp                         from 'gulp';
-import gulpTasks                    from 'typhonjs-core-gulptasks';
+import gulp       from 'gulp';
+import gulpTasks  from 'typhonjs-core-gulptasks';
 
 // Import all tasks and set `rootPath` to the base project path and `srcGlob` to all JS sources in `./src`.
 gulpTasks(gulp,
